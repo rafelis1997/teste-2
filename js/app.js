6bca0883ef0ac018ce9aa5e2d95b73969890d192
Rafheros <rafaelheros80@gmail.com>
2022-02-10 02:26:11 -0300

    teste 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "%UNITY_WEBGL_BUILD_URL%");  
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Ar teste.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
